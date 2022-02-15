f4b200d3abfb6b9fc31c96d0ba798c7cbd4a37cd
minbo2002 <68101836+minbo2002@users.noreply.github.com>
2022-02-16 04:03:32 +0900

    [feat] 회원관리 예제 (등록, 조회를 MVC로 개발)
    DB접근 : 순수Jdbc, JdbcTemplate, JPA, 스프링데이터 JPA (H2 DataBase사용)

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -19,4 +20,31 @@ public class HelloController {
         model.addAttribute("name", name);
         return "hello-template";
     }
+
+    @GetMapping("hello-string")
+    @ResponseBody   // http에서 body의 data를 직접 넣어주겠다
+    public String helloString(@RequestParam("name") String name) {
+
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;   // 객체반환 --> 디폴트로 JSON 방식으로 응답
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+   }
 }
